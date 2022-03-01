431f153eacf5869c551d1937eb61d6a374bd73c5
Daniel Franke <d.t.l.franke@student.tudelft.nl>
2022-03-01 15:30:16 +0100

    I deleted unused imports so that the pipeline does not fail anymore

--- commons/src/main/java/commons/Question.java
+++ commons/src/main/java/commons/Question.java
@@ -17,12 +17,10 @@ package commons;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
