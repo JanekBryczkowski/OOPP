68ea9f3ffc3a9268b53cf0bfa1de34be7963e45c
Daniel Franke <d.t.l.franke@student.tudelft.nl>
2022-03-03 14:12:11 +0100

    This branch/ commit is for testing the backend of the application

---
+++ server/src/main/java/server/api/UserController.java
@@ -0,0 +1,54 @@
+package server.api;
+
+import commons.Question;
+import server.database.QuestionRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+//    private final QuestionRepository repo;
+//
+//    public QuestionController(QuestionRepository repo) {
+//        this.repo = repo;
+//    }
+
+
+
+//    @GetMapping("/")
+//    public List<Question> getQuestions() {
+//        return repo.findAll();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Question> getById(@PathVariable("id") long id) {
+//        if (id < 0 || !repo.existsById(id)) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//        return ResponseEntity.ok(repo.getById(id));
+//    }
+//
+//    private static boolean isNullOrEmpty(String s) {
+//        return s == null || s.isEmpty();
+//    }
+//
+//    @PostMapping(path = { "", "/" })
+//    public ResponseEntity<Question> add(@RequestBody Question question) {
+//
+//        if (question==null || isNullOrEmpty(question.title)) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//
+//        Question saved = repo.save(question);
+//        return ResponseEntity.ok(saved);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    void deleteQuestion(@PathVariable long id) {
+//        repo.deleteById(id);
+//    }
+}
