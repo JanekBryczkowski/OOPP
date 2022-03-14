f50a6170a7b9c6ccba42690b51e33ce544e1d082
Hanna Heinczinger <H.Heinczinger-1@student.tudelft.nl>
2022-03-14 18:50:11 +0100

    Storing the scores in a database

--- server/src/main/java/server/database/UserScoreRepository.java
+++ server/src/main/java/server/database/UserScoreRepository.java
@@ -1,13 +1,12 @@
 package server.database;
 
-import commons.Activity;
 import commons.Scores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface UserScoreRepository extends JpaRepository<Activity, Long> {
+public interface UserScoreRepository extends JpaRepository<Scores, Long> {
 
     @Query(value = "SELECT * FROM Scores ORDER BY DESC", nativeQuery = true)
     List<Scores> getThreeRandom();
