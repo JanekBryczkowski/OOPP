75d09c5a2856560c11dbbb570c6c44afffa5b079
Pavlos <P.Markesinis@student.tudelft.nl>
2022-03-24 14:32:07 +0100

    I added functionality of the admin to inspect all the questions from database

--- client/src/main/java/client/scenes/AdminCtrl.java
+++ client/src/main/java/client/scenes/AdminCtrl.java
@@ -4,12 +4,17 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.Activity;
 
+import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +23,9 @@ public class AdminCtrl {
     private final ServerUtils server;
     private final GameCtrl gameCtrl;
     List<Activity> activityList = new ArrayList<>();
+
+    @FXML
+    private ScrollPane scroll;
 
 
     //Connecting the page with the server, and the GameCtrl
@@ -28,6 +36,7 @@ public class AdminCtrl {
     }
 
     public void setTable() {
+        System.out.println("xd");
         activityList.addAll(server.showAll());
         showAllActivities(activityList);
     }
@@ -36,43 +45,44 @@ public class AdminCtrl {
         VBox vbox = new VBox();
         for (Activity activity : activityList) {
             AnchorPane anchorPane = new AnchorPane();
-            anchorPane.setMaxHeight(30);
-            anchorPane.setMinHeight(30);
-            anchorPane.setMinHeight(30);
-            anchorPane.setMaxWidth(347);
-            anchorPane.setMinWidth(347);
-            anchorPane.setPrefWidth(347);
-            Label activityText = new Label();
-            activityText.setMaxHeight(30);
-            activityText.setMinHeight(30);
-            activityText.setPrefHeight(30);
-            activityText.setMaxWidth(200);
-            activityText.setMinWidth(200);
-            activityText.setPrefWidth(200);
-            activityText.setLayoutX(0);
-            activityText.setLayoutY(0);
-            activityText.setStyle("-fx-font-size: 16;");
-            activityText.setAlignment(Pos.CENTER);
-            Label Consumption = new Label();
-            Consumption.setMaxHeight(30);
-            Consumption.setMinHeight(30);
-            Consumption.setPrefHeight(30);
-            Consumption.setMaxWidth(147);
-            Consumption.setMinWidth(147);
-            Consumption.setPrefWidth(147);
-            Consumption.setLayoutX(200);
-            Consumption.setLayoutY(0);
-            Consumption.setStyle("-fx-font-size: 16;");
-            Consumption.setAlignment(Pos.CENTER);
-            anchorPane.getChildren().add(activityText);
-            anchorPane.getChildren().add(Consumption);
+            anchorPane.setMaxHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            anchorPane.setMinHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            anchorPane.setMinHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            anchorPane.setMaxWidth(620);
+            anchorPane.setMinWidth(620);
+            anchorPane.setPrefWidth(620);
+            Label activityLabel = new Label(activity.title);
+            activityLabel.setWrapText(true);
+            activityLabel.setTextAlignment(TextAlignment.CENTER);
+            activityLabel.setPrefHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            activityLabel.setMaxHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            activityLabel.setMinHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            activityLabel.setMaxWidth(350);
+            activityLabel.setMinWidth(350);
+            activityLabel.setPrefWidth(350);
+            activityLabel.setLayoutX(0);
+            activityLabel.setLayoutY(0);
+            activityLabel.setStyle("-fx-font-size: 16;");
+            activityLabel.setAlignment(Pos.CENTER);
+
+            Label consumption = new Label(String.valueOf(activity.consumption));
+            consumption.setMaxHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            consumption.setMinHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            consumption.setPrefHeight(30 * ((int) (activity.title.length() / 40) + 1));
+            consumption.setMaxWidth(190);
+            consumption.setMinWidth(190);
+            consumption.setPrefWidth(190);
+            consumption.setLayoutX(350);
+            consumption.setLayoutY(0);
+            consumption.setStyle("-fx-font-size: 16;");
+            consumption.setAlignment(Pos.CENTER);
+            anchorPane.getChildren().add(activityLabel);
+            anchorPane.getChildren().add(consumption);
             vbox.getChildren().add(anchorPane);
         }
-//        scroll.setContent(vbox);
+        scroll.setContent(vbox);
 
     }
-
-
 
 
 }
