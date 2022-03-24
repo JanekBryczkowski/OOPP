1fa8189120b24ab4a186842ea19252a6bf603daf
Jan Bryczkowski <J.M.Bryczkowski@student.tudelft.nl>
2022-03-24 16:58:50 +0100

    Deleting unused imports

--- client/src/main/java/client/scenes/SplashScreenCtrl.java
+++ client/src/main/java/client/scenes/SplashScreenCtrl.java
@@ -12,18 +12,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.BoxBlur;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
-import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
-import org.checkerframework.checker.units.qual.C;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
 public class SplashScreenCtrl {
 
